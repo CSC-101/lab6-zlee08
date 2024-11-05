c31203c4ed73b0bdd11c7ad1aa11dd575ca830ac
Zoe Lee <zlee08@calpoly.edu>
2024-11-05 02:45:20 -0800

    Final commit

--- lab6_tests.py
+++ lab6_tests.py
@@ -64,19 +64,56 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
+    def test_selection_sort1(self):
+        input = [(data.Book(["Zoe Lee"],"Wonder")), (data.Book(["Tom Holland"],"Moby Dick")), (data.Book(["Bob Ross"],"Outsiders"))]
+        expected = [(data.Book(["Tom Holland"], "Moby Dick")), (data.Book(["Bob Ross"],"Outsiders")), (data.Book(["Zoe Lee"],"Wonder"))]
+        result = lab6.selection_sort_books(input)
+        self.assertEqual(result, expected)
 
+    def test_selection_sort2(self):
+        input = [(data.Book(["JK Rowling"], "Harry Potter")), (data.Book(["John Steinbeck"],"Grapes of Wrath")), (data.Book(["F. Scott Fitzgerald"], "Great Gatsby"))]
+        expected = [(data.Book(["John Steinbeck"],"Grapes of Wrath")), (data.Book(["F. Scott Fitzgerald"], "Great Gatsby")),(data.Book(["JK Rowling"], "Harry Potter")) ]
+        result = lab6.selection_sort_books(input)
+        self.assertEqual(result, expected)
 
     # Part 2
+    def test_swap_case1(self):
+        input = "Hello World!"
+        expected = "hELLO wORLD!"
+        result = lab6.swap_case(input)
+        self.assertEqual(result, expected)
 
+    def test_swap_case2(self):
+        input = "Zoe Lee"
+        expected = "zOE lEE"
+        result = lab6.swap_case(input)
+        self.assertEqual(result, expected)
 
     # Part 3
+    def test_str_translate1(self):
+        input = "Zoe Lee"
+        expected = "Zoo Loo"
+        result = lab6.str_translate(input, "e", "o")
+        self.assertEqual(result, expected)
 
+    def test_str_translate2(self):
+        input = "abcdcba"
+        expected = "xbcdcbx"
+        result = lab6.str_translate(input,"a", "x")
+        self.assertEqual(result, expected)
 
     # Part 4
+    def test_histogram1(self):
+        input = "the happy happy penguin"
+        expected = {"the":1, "happy":2, "penguin":1}
+        result = lab6.histogram(input)
+        self.assertEqual(result, expected)
 
-
-
-
+    def test_histogram2(self):
+        input = "Zoe Lee"
+        expected = {"Zoe":1, "Lee":1}
+        result = lab6.histogram(input)
+        self.assertEqual(result,expected)
 
 if __name__ == '__main__':
     unittest.main()
